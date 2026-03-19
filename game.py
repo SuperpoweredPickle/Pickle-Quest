cb0936a54abe7b6e4bba9cbe091c46b84d264e9e
SuperpoweredPickle <25StaponaiteV@smrt.bristol.sch.uk>
2026-03-19 07:15:52 +0000

    Create game.py

---
+++ game.py
@@ -0,0 +1,7 @@
+Name = input("Enter your name! ")
+print(f"Greetings {Name} Welcome to your adventure! ")
+start = input ("Would you rather play the game or perish >:( ")
+if start == 'play':
+    print ("Great! Let's play!")
+else:  
+    print("Lame. Okay you're existence was wiped off the earth now")
